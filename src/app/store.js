03319d828a5a23bfb98e27ddc7be8342696dd5f6
Nicholas Skupien <nicholasskupien@gmail.com>
2022-08-07 23:30:15 -0400

    fixing some errors thrown by prettier

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import catalogReducer from '../features/catalog/catalogSlice'
-import shoppingCartReducer from '../features/catalog/shoppingCartSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import catalogReducer from "../features/catalog/catalogSlice";
+import shoppingCartReducer from "../features/catalog/shoppingCartSlice";
 
 export default configureStore({
   reducer: {
-      catalog: catalogReducer,
-      cart: shoppingCartReducer,
+    catalog: catalogReducer,
+    cart: shoppingCartReducer,
   },
-})
+});
