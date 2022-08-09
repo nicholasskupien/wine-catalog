5e3a11aa7032238969931395435e6c5753343217
Nicholas Skupien <nicholasskupien@gmail.com>
2022-08-08 20:56:43 -0400

    small changes to stylesheets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import LeftNavigation from "./components/LeftNavigation/LeftNavigation";
 import CatalogContainer from "./components/catalog/CatalogContainer/CatalogContainer";
-import "./App.css";
+import styles from "./App.module.scss";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 
 function App() {
   return (
-    <div className="app-container">
+    <div className={styles.AppContainer}>
       <LeftNavigation></LeftNavigation>
       <CatalogContainer></CatalogContainer>
       <ShoppingCart></ShoppingCart>
